ec6815c434b1e4074187f38fb2255871f0aa1607
MXXXXXS <relife_abyss@qq.com>
2022-05-08 14:44:32 +0800

    feat: 第一章, 华氏摄氏对照表, 浮点数版本, 使用#define

--- chapter1/hello_world.c
+++ chapter1/hello_world.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 main()
 {
+
   float fahr, celsius;
-  float lower, upper, step;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  fahr = lower;
-  while (fahr <= upper)
+  for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
   {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr += step;
   }
 }
